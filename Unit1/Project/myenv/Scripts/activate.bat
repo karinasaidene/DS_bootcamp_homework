1682d985b62a111bb1d9726a234d7042a6d4540f
karinasaidene <jk_saidene@esi.dz>
2022-02-22 11:23:37 +0100

    fixed

--- Unit1/Project/myenv/Scripts/activate.bat
+++ Unit1/Project/myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Dell\Desktop\DS_bootcamp_homework\Unit1\Project\prep_unit_project\myenv
+set VIRTUAL_ENV=C:\Users\ZenBook\Documents\DS_bootcamp_homework\DS_bootcamp_homework\Unit1\Project\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
